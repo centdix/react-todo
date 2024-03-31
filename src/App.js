9ed0646a59b316242dba70ea5ee5c23c0e570697
Farhad <farhadg@the-ifc.com>
2024-03-31 16:22:04 +0200

    simple todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='container'>
+        <h1>Todo App</h1>
+        <input
+          type="text"
+          placeholder="Enter a todo"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              setTodos([...todos, e.target.value]);
+              e.target.value = "";
+            }
+          }}
+          className='input'
+        />
+        <div
+          className='list'
         >
-          Learn React
-        </a>
-      </header>
+          {todos.map((todo, index) => (
+            <div
+              key={index}
+              className='list-item'
+            >
+                <span className="list-text">{todo}</span>
+                <button
+                  onClick={() => {
+                    const newTodos = [...todos];
+                    newTodos.splice(index, 1);
+                    setTodos(newTodos);
+                  }}
+                  className='delete-button'
+                >delete</button>
+            </div>
+          ))}
+        </div>
     </div>
   );
 }
